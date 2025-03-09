74ee9353db440608b37d310c6202d6ca06e77a60
hamza <hamzaiddou2004@gmail.com>
2025-03-09 17:40:27 +0100

    finish this small project

---
+++ Text to Speech Converter/script.js
@@ -0,0 +1,24 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voicesSelect = document.querySelector("select");
+
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+
+    voices.forEach((voices, i) => (voicesSelect.options[i] = new Option(voices.name, i)));
+
+}
+
+voicesSelect.addEventListener("change", ()=>{
+    speech.voice = voices[voicesSelect.value]
+})
+
+document.querySelector("button").addEventListener("click", ()=>{
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
